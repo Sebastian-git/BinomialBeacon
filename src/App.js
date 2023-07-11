52542df770c3cf827da070b9e818bc1b66e66fab
Sebastian-git <sebastian01cevallos@gmail.com>
2023-07-11 05:13:39 -0700

    New UI with variables for Binomial Options Pricing Model

--- src/App.js
+++ src/App.js
@@ -1,59 +1,47 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import {
-  ResponsiveContainer, LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend
-} from 'recharts';
 
 const App = () => {
-  const [stockData, setStockData] = useState([]);
-  const [optionData, setOptionData] = useState([]);
+  const [stockPrice, setStockPrice] = useState(0);
+  const [strikePrice, setStrikePrice] = useState(2.5); // Assume a strike price for now
+  const [timeToExpiry, setTimeToExpiry] = useState(365); // Assume 365 days to expiration for now
+  const [riskFreeRate, setRiskFreeRate] = useState(0.01); // Assume a 1% risk-free rate
+  const [volatility, setVolatility] = useState(0.2); // Assume 20% volatility
+  const [steps, setSteps] = useState(100); // Assume 100 steps in the binomial model
+  const [optionPrice, setOptionPrice] = useState(0);
 
   const fetchStockData = async () => {
     try {
       const response = await axios.get('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=AAPL&interval=5min&apikey=CC1EJTK0U40Z2M9C');
-      const data = Object.entries(response.data['Time Series (5min)']).map(([date, item]) => ({
-        date,
-        stockPrice: parseFloat(item['4. close'])
-      })).slice(0, 50).reverse();
-      setStockData(data);
+      const data = Object.entries(response.data['Time Series (5min)']).map(([date, item]) => parseFloat(item['4. close']));
+      const currentStockPrice = data[data.length - 1];
+      setStockPrice(currentStockPrice);
     } catch (error) {
       console.error('Error fetching stock data: ', error);
     }
   };
 
-  const fetchOptionData = async () => {
-    try {
-      const response = 0// await axios.get('https://api.polygon.io/v3/reference/options/symbols?apiKey=kmsrYoQHhvAPTv2_Wk7GQwtwSmyk_epK');
-      const res = await axios.get('https://api.polygon.io/v3/reference/options/contracts/O:EVRI240119C00002500?apiKey=kmsrYoQHhvAPTv2_Wk7GQwtwSmyk_epK');
-      console.log("res:", res)
-      const data = response.data.results.map((item) => ({
-        date: item.list_date,
-        optionPrice: item.last
-      })).slice(0, 50).reverse();
-      setOptionData(data);
-    } catch (error) {
-      console.error('Error fetching option data: ', error);
-    }
+  const calculateOptionPrice = () => {
+    // TODO: Implement the binomial options pricing model here
+    // For now, let's just set the option price to be the stock price
+    setOptionPrice(stockPrice);
   };
 
   useEffect(() => {
     fetchStockData();
-    fetchOptionData();
-  }, []);
+    calculateOptionPrice();
+  }, [stockPrice, strikePrice, timeToExpiry, riskFreeRate, volatility, steps]);
 
   return (
-    <div style={{ width: '100%', height: 500 }}>
-      <ResponsiveContainer>
-        <LineChart data={stockData}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="date" tick={{ fontSize: 12 }} height={70} angle={-45} interval={0} />
-          <YAxis />
-          <Tooltip />
-          <Legend verticalAlign="top" height={36}/>
-          <Line type="monotone" dataKey="stockPrice" stroke="#8884d8" activeDot={{ r: 8 }} />
-          <Line type="monotone" dataKey="optionPrice" stroke="#82ca9d" />
-        </LineChart>
-      </ResponsiveContainer>
+    <div>
+      <h1>Binomial Options Pricing Model</h1>
+      <p>Stock Price: {stockPrice}</p>
+      <p>Strike Price: {strikePrice}</p>
+      <p>Time to Expiry: {timeToExpiry} days</p>
+      <p>Risk-Free Rate: {riskFreeRate * 100}%</p>
+      <p>Volatility: {volatility * 100}%</p>
+      <p>Steps: {steps}</p>
+      <h2>Option Price: {optionPrice}</h2>
     </div>
   );
 };
