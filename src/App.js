9da72783a8146a2b6c01684cb1a375553ad9c735
Sebastian-git <sebastian01cevallos@gmail.com>
2023-07-21 17:40:08 -0700

    Code makes binomial tree, just missing a few edge connections
    * Edges belonging to duplicate children and their top parent node doesn't exist yet, will continue to try and fix this one later but want to save progress
    * Graph scales with number of steps more accurately

--- src/App.js
+++ src/App.js
@@ -7,10 +7,8 @@ function round(value, decimals) {
 }
 
 const upSize = (stdDev, deltaT) => { return Math.exp((stdDev * 0.01) * Math.sqrt(deltaT)) };
+
 const downSize = (stdDev, deltaT) => { return 1.0/(Math.exp((stdDev * 0.01) * Math.sqrt(deltaT))) };
-
-
-// Step 3: Calculate option payoff at expiration (works for both curPrice=downPrice and curPrice=upPrice)
 
 const callBuyPayoff = (curPrice, strikePrice) => { return Math.max(curPrice - strikePrice, 0) }
 
@@ -20,7 +18,7 @@ let stdDev = 19 // SPY std dev for past 3 years
 let stockPrice = 100
 let strikePrice = 100
 let rfr = 0.0375; // Risk free rate
-let totalTime = 1; // 1.5 years
+let totalTime = 10; // n years
 
 function App() {
 
@@ -56,178 +54,131 @@ function App() {
 
   let id = 0;
   let map = {};
-  let duplicates = []
+  let siblings = {}
 
-  // Modified dummy data function
-  const createData = (deltaT, curSteps, price, parent, root) => {
-
-    let payoff = putBuyPayoff(price, strikePrice);  //callBuyPayoff(price, strikePrice);
-    
-    // For child nodes, option value is just payoff
-    let optionValue = payoff
-
-    // At children of tree, calculate payoff
-    if (curSteps === 0) {
-      return { name: `${round(price, 3)} (${round(payoff, 3)}) ${curSteps}`, optionValue: optionValue }; 
+  const createGraphData = (deltaT, curSteps, price, id = 0, parentId = null) => {
+    let payoff = callBuyPayoff(price, strikePrice);
+    let optionValue = payoff;
+  
+    let x = (steps - curSteps) * 300 * steps;
+    let y = id * 100 * steps;
+  
+    let nodeName = `${round(price, 3)} (${round(optionValue, 3)}) \n${curSteps}-${id}`;
+  
+    if (map[nodeName]) {
+      return { nodes: [], links: [] };
     }
-
-    // Price for nodes in futures where stock goes up and down
-    let upPrice = price * upMove
-    let downPrice = price * downMove
-
-    // Create nodes for graph recursively
-    let upChild = createData(deltaT, curSteps - 1, upPrice, curSteps, root);
-    let downChild = createData(deltaT, curSteps - 1, downPrice, curSteps, root);
-    
-    // Expected value ( (probability of up * up price) + (probability of down * down price) )
-    let expectedValue = (riskNeutralProbability * upChild.optionValue) + ((1-riskNeutralProbability) * downChild.optionValue)
-
-    // Discount expected value by risk free rate
-    let discountedValue = expectedValue * Math.exp((-1) * rfr * deltaT); // THIS IS THE CALL OPTIONS VALUE
-
-    // Price if option was sold now = strike price - stock price at node
-    let earlyExercise = payoff;
-    
-    // For non-child nodes, optionValue is discounted value or price if option was sold now
-    optionValue = Math.max(earlyExercise, discountedValue)
-
+  
     let node = {
-      name: `${round(price, 3)} (${round(optionValue, 3)}) ${curSteps}`,
-      children: [upChild, downChild],
-      optionValue: optionValue
+      name: nodeName,
+      id: nodeName,
+      optionValue: optionValue, 
+      x: x,
+      y: y,
+    };
+  
+    map[nodeName] = node;
+  
+    if (curSteps === 0) {
+      return {
+        nodes: [node],
+        links: []
+      }; 
+    }
+  
+    let upPrice = price * upMove;
+    let downPrice = price * downMove;
+  
+    let upResult = createGraphData(deltaT, curSteps - 1, upPrice, id + 1, nodeName);
+    let downResult = createGraphData(deltaT, curSteps - 1, downPrice, id - 1, nodeName);
+  
+    let linkToUpChild = {
+      source: node.name,
+      target: upResult.nodes[0]?.name
+    };
+  
+    let linkToDownChild = {
+      source: node.name,
+      target: downResult.nodes[0]?.name
     };
 
-    if (map[node.name] && map[node.name].parent === parent) {
-      if (parent) {
-        duplicates.push(node.name)
-        console.log("added duplicate", node.name)
-      }
-    } else {
-      node.parent = parent
-      map[node.name] = node
-    }
-    return node;
+    siblings[upResult.nodes[0]?.name] = downResult.nodes[0]?.name
+    siblings[downResult.nodes[0]?.name] = upResult.nodes[0]?.name
+  
+    return {
+      nodes: [node, ...upResult.nodes, ...downResult.nodes],
+      links: [linkToUpChild, linkToDownChild, ...upResult.links, ...downResult.links]
+    };
   };
 
-  // If duplicate prices, set them to be the same node (for one of them, set parent.right = other parent.right, then delete the other extra one). Probably just going to need new graph library for this
   let root = {
     name: `${round(stockPrice, 3)} (0) 0-0`,
     children: [],
     optionValue: 0
   };
 
-  const [data, setData] = useState(root);
-
-  const removeDuplicates = (root) => {
-
-    let level = [root];
-    let leftParent = null;
-    let rightParent = null;
-    let deleteChild = null;
-    let keepChild = null;
-
-    const fillInfo = (cur, child, index) => {
-      // Always delete left child
-      if (index == 0) {
-        leftParent = cur;
-        deleteChild = child;
-      }
-      else {
-        rightParent = cur;
-        keepChild = child;
-      }
-    }
-
-    while (level.length > 0) {
-      let newLevel = []
-      while (level.length > 0) {
-        let cur = level.pop();
-        // console.log("searching", cur)
-        if (!cur.children || cur.children.length === 0) continue;
-        let left = cur.children[0], right = cur.children[1];
-        if (left.name === duplicates[0] || right.name === duplicates[0]) {
-          if (left.name === duplicates[0]) fillInfo(cur, left, 0);
-          if (right.name === duplicates[0]) fillInfo(cur, right, 1);
-          if (keepChild !== null) {
-            console.log("ready:", leftParent)
-            leftParent.children[0] = 0
-          }
-        }
-        newLevel.push(left)
-        newLevel.push(right)
-      }
-      level = newLevel
-    }
-
-  }
+  const [nodes, setNodes] = useState([root]);
+  const [links, setLinks] = useState([]);
 
   useEffect(() => {
-    // Update data whenever steps, upMove, downMove, or riskNeutralProbability changes
-    root = createData(deltaT, steps, stockPrice, null, root);
-    if (duplicates.length > 0) {
-      console.log("HERE", duplicates)
-      removeDuplicates(root)
-    }
-    // console.log("root:", root)
-    setData(root);
-  }, [steps, upMove, downMove, riskNeutralProbability]);
+    let graphData = createGraphData(deltaT, steps, stockPrice, null, root);
+    graphData.nodes.forEach(node => {
+      node.name = node.name.replace(/\n.*$/, '');
+    });
+    setNodes(graphData.nodes);
+    setLinks(graphData.links);
+  }, [steps, upMove, downMove, riskNeutralProbability]);  
 
-
-// Then use the data state variable in getOption
-const getOption = () => {
-  return {
-    tooltip: {
-      trigger: 'item',
-      triggerOn: 'mousemove'
-    },
-    series: [
-      {
-        type: 'tree',
-        data: [data], // Use the data state variable here
-        top: '1%',
-        left: '7%',
-        bottom: '1%',
-        right: '20%',
-        symbolSize: 15,
-        label: {
-          position: [50, 30],
-          verticalAlign: 'middle',
-          align: 'right',
-          fontSize: 15
-        },
-        leaves: {
+  const getGraphOption = () => {
+    return {
+      tooltip: {},
+      animationDurationUpdate: 300,
+      animationEasingUpdate: 'quinticInOut',
+      series: [
+        {
+          type: 'graph',
+          layout: 'none',
+          symbolSize: 30,
+          roam: true,
           label: {
-            position: 'right',
-            verticalAlign: 'middle',
-            align: 'left'
+            show: true,
+            color: "black",
+            fontSize: 20 - (steps/2),
+            position: [-35, 35]
+          },
+          edgeSymbol: ['circle', 'arrow'],
+          edgeSymbolSize: [4, 10],
+          edgeLabel: {
+            fontSize: 20 - (steps/4),
+          },
+          data: nodes,
+          links: links,
+          lineStyle: {
+            width: 4,
+            curveness: 0
           }
-        },
-        expandAndCollapse: false,
-        animationDuration: 300,
-        animationDurationUpdate: 750
-      }
-    ]
+        }
+      ]
+    };
   };
-};
 
-
-return (
-  <div className="App">
-    <h1>Binomial Tree Model Visualization</h1>
-    <div>
+  return (
+    <div className="App">
+      <h1>Binomial Tree Model Visualization</h1>
       <div>
-        <label>Timesteps: </label>
-        <input type="range" min="1" max="5" value={steps} 
-          className="slider" onChange={(e) => updateTimeStep(Number(e.target.value))} />
-        <span>{steps}</span>
-        <p>Delta T: {deltaT}, rfr: {rfr}, riskNeutralProbability: {riskNeutralProbability} </p>
-        <p>stdDev: {stdDev}, strikePrice: {strikePrice}, stockPrice: {stockPrice} </p>
-        <p>u: {upMove}, d: {downMove} </p>
+        <div>
+          <label>Timesteps: </label>
+          <input type="range" min="1" max={totalTime} value={steps} 
+            className="slider" onChange={(e) => updateTimeStep(Number(e.target.value))} />
+          <span>{steps}</span>
+          <p>Delta T: {deltaT}, rfr: {rfr}, riskNeutralProbability: {riskNeutralProbability} </p>
+          <p>stdDev: {stdDev}, strikePrice: {strikePrice}, stockPrice: {stockPrice} </p>
+          <p>u: {upMove}, d: {downMove} </p>
+        </div>
       </div>
+      <ReactECharts option={getGraphOption()} style={{height: '80vh', width: '100%'}}/>
     </div>
-    <ReactECharts option={getOption()} style={{height: '80vh', width: '100%'}}/>
-  </div>
-);
+  );
 }
 
 export default App;
